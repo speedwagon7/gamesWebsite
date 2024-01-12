d53ae585983359f6875881f3a8a6c09c55e7acb1
hayden.poon <hayden.poon@fdmgroup.com>
2024-01-12 19:27:05 +0000

    added user model and repo

---
+++ backend-springboot/demo/src/main/java/com/user/model/User.java
@@ -0,0 +1,82 @@
+package com.user.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="accounts")
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+    
+    @Column
+    private String username;
+    
+
+    /**
+     * @return Long return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return String return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return String return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
