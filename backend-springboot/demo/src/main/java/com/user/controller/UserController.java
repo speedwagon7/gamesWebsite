b1ab1924dda67847657cb39b672c91596edd9e8a
hayden.poon <hayden.poon@fdmgroup.com>
2024-01-19 14:17:13 +0000

    added lombok annotations and removed excess code

--- backend-springboot/demo/src/main/java/com/user/controller/UserController.java
+++ backend-springboot/demo/src/main/java/com/user/controller/UserController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,7 +22,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
-@RequestMapping("api/v1/users")
+@RequestMapping("api")
 @AllArgsConstructor
 public class UserController {
 
@@ -31,9 +33,19 @@ public class UserController {
         return userRepository.getReferenceById(Id);
     }
     
-    @PostMapping("/addUser")
-    public User addUser(@RequestBody User newUser) {     
-        return userRepository.save(newUser);
+    @PostMapping("/registerUser")
+    public ResponseEntity<User> addUser(@RequestBody User newUser) {
+        System.out.println("testing");
+        try {
+            // Validate and save the user
+            User savedUser = userRepository.save(newUser);
+    
+            // Return a ResponseEntity with the saved user and HTTP status CREATED
+            return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+        } catch (Exception e) {
+            // Handle exceptions and return an appropriate response
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
     
     
--- backend-springboot/demo/src/main/java/com/user/model/User.java
+++ backend-springboot/demo/src/main/java/com/user/model/User.java
@@ -6,7 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name="users")
 public class User {
@@ -22,62 +30,5 @@ public class User {
     
     @Column(nullable = false, length = 20)
     private String username;
-    
-
-    /**
-     * @return Long return the id
-     */
-    public Long getId() {
-        return id;
-    }
-
-    /**
-     * @param id the id to set
-     */
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    /**
-     * @return String return the email
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * @param email the email to set
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /**
-     * @return String return the password
-     */
-    public String getPassword() {
-        return password;
-    }
-
-    /**
-     * @param password the password to set
-     */
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    /**
-     * @return String return the username
-     */
-    public String getUsername() {
-        return username;
-    }
-
-    /**
-     * @param username the username to set
-     */
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
 }
