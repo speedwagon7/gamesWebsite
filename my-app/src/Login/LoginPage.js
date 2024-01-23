973bb3f051ff5cab90acd9f24acc1c319662b845
hayden.poon <hayden.poon@fdmgroup.com>
2024-01-23 02:27:44 +0000

    sending post request using form submit button

--- my-app/src/Login/LoginPage.js
+++ my-app/src/Login/LoginPage.js
@@ -1,16 +1,30 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import './LoginPage.css';
-
+import axios from 'axios';
+import { useState } from 'react';
 
 function LoginPage(){
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleClick = async() => {
+        try {
+            const response = await axios.post("http://localhost:8080/api/users/register")
+        } catch (error) {
+            console.error('Error submitting form:', error);
+        }
+        
+
+    }
     return(
         <div>
             <div className='LoginBox'>
                 <Form>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label> Email Address </Form.Label>
-                            <Form.Control type="email" placeholder="Enter email"/>
+                            <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
                             <Form.Text className="text-muted">
                                 We'll never share your email with anyone else.
                             </Form.Text>
@@ -18,7 +32,7 @@ function LoginPage(){
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label> Password </Form.Label>
-                            <Form.Control type="password" placeholder="Enter Password"/>
+                            <Form.Control type="password" placeholder="Enter Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicCheckbox">
@@ -26,7 +40,7 @@ function LoginPage(){
                     </Form.Group>
 
                     <Form.Group className="mb-3">
-                        <Button variant="primary" type="submit">
+                        <Button onClick={handleClick} variant="primary" type="submit">
                             Submit
                         </Button>
                     </Form.Group>
